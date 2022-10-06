ad8df34dc478d010ec655d313731c4a9e0906aee
Josue0625 <jcampos@ufpso.edu.co>
2022-10-06 11:14:00 -0500

    Creacion del proyecto, cargue de las dependencias

---
+++ src/main/java/com/tercer_ejemplo_parcial/josue/JosueApplication.java
@@ -0,0 +1,13 @@
+package com.tercer_ejemplo_parcial.josue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosueApplication.class, args);
+	}
+
+}
